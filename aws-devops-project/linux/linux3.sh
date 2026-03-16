4ca1f37e97a120f4c23e92b1ad3e3521786172b5
kancharla kavya <kancharlakavya2403@gmail.com>
2026-03-16 20:14:40 +0530

    linux3.sh

--- aws-devops-project/linux/linux3.sh
+++ aws-devops-project/linux/linux3.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-set -e
 
-# Update packages
-yum update -y
+dnf update -y
+dnf install nginx python3 -y
 
-# Install python and nginx
-yum install -y python3
-amazon-linux-extras install nginx1 -y
-
-# Start nginx
 systemctl start nginx
 systemctl enable nginx
 
-# Create python script
 echo "print('Python installed successfully on this EC2 instance')" > /home/ec2-user/test.py
 
-# Run python and display output on webpage
 python3 /home/ec2-user/test.py > /usr/share/nginx/html/index.html
