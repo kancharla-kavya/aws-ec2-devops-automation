1e75d6d2336afad4d914b549a6e492627cff98b3
kancharla kavya <kancharlakavya2403@gmail.com>
2026-03-16 20:13:56 +0530

    linux2.sh

--- aws-devops-project/linux/linux2.sh
+++ aws-devops-project/linux/linux2.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
-set -e
 
-# Update packages
-yum update -y
+dnf update -y
+dnf install nginx curl -y
 
-# Install nginx and curl
-amazon-linux-extras install nginx1 -y
-yum install -y curl
-
-# Start nginx
 systemctl start nginx
 systemctl enable nginx
 
-# Create folder for image
 mkdir -p /usr/share/nginx/html/images
 
-# Download an image
 curl -o /usr/share/nginx/html/images/aws.png https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg
 
-# Create webpage displaying image
 echo "<html>
-<head>
-<title>Linux Instance 2</title>
-</head>
-<body style='text-align:center;font-family:Arial'>
+<body style='text-align:center'>
 <h1>Linux Instance 2</h1>
 <p>This server displays an image hosted on EC2</p>
 <img src='images/aws.png' width='300'>
