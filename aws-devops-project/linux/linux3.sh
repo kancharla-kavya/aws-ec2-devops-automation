f013ce149d52fa1c2fdf3793c28456e1b03dc970
kancharla-kavya <kancharlakavya2403@gmail.com>
2026-03-27 12:01:12 +0000

    updated linux-3

--- aws-devops-project/linux/linux3.sh
+++ aws-devops-project/linux/linux3.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
 
-sleep 30
+ 
+
+# Log everything (for debugging if needed)
+
+exec > /var/log/user-data.log 2>&1
+
+ 
+
+# Wait until network + repos ready
+
+sleep 40
+
+ 
+
+# Install nginx + python
 
 dnf update -y
-dnf install nginx python3 
 
-systemctl start nginx
+dnf install -y nginx python3
+
+ 
+
+# Remove default nginx page
+
+rm -f /usr/share/nginx/html/index.html
+
+ 
+
+# Create your custom page using Python
+
+echo "print('Python Installed Successfully on EC2 ')" > /home/ec2-user/app.py
+
+python3 /home/ec2-user/app.py > /usr/share/nginx/html/index.html
+
+ 
+
+# Start nginx AFTER content is ready
+
 systemctl enable nginx
-
-echo "<h1 style='text-align:center'> Python Installed Successfully on this EC2 instance </h1>" > /usr/share/nginx/html/index.html 
 
 systemctl restart nginx
