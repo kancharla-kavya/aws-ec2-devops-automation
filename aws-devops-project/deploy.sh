3043e1c5a1a9379a7c2e987fca879ba43f275378
kancharla-kavya <kancharlakavya2403@gmail.com>
2026-03-27 12:41:38 +0000

    deploy.sh is updated

--- aws-devops-project/deploy.sh
+++ aws-devops-project/deploy.sh
@@ -1,58 +1,119 @@
 #!/bin/bash
 
+set -e
+
+ 
+
 echo "===================================="
+
 echo "Starting AWS EC2 Automation Deploy"
+
 echo "===================================="
+
+ 
 
 echo "Launching Linux Instance 1"
+
 aws ec2 run-instances \
+
 --image-id ami-0f559c3642608c138 \
+
 --count 1 \
+
 --instance-type t3.micro \
+
 --key-name devops-key \
+
 --security-groups devops-sg \
+
 --user-data file://linux/linux1.sh \
---tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Linux-Nginx}]'
+
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Linux-Nginx}]'
+
+ 
 
 echo "Launching Linux Instance 2"
+
 aws ec2 run-instances \
+
 --image-id ami-0f559c3642608c138 \
+
 --count 1 \
+
 --instance-type t3.micro \
+
 --key-name devops-key \
+
 --security-groups devops-sg \
+
 --user-data file://linux/linux2.sh \
---tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Linux-docker}]'
+
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Linux-Docker}]'
+
+ 
 
 echo "Launching Linux Instance 3"
+
 aws ec2 run-instances \
+
 --image-id ami-0f559c3642608c138 \
+
 --count 1 \
+
 --instance-type t3.micro \
+
 --key-name devops-key \
+
 --security-groups devops-sg \
+
 --user-data file://linux/linux3.sh \
---tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Linux-Python}]'
+
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Linux-Python}]'
+
+ 
 
 echo "Launching Windows Instance 1"
+
 aws ec2 run-instances \
+
 --image-id ami-05773800d221ec923 \
+
 --count 1 \
+
 --instance-type t3.micro \
+
 --key-name devops-key \
+
 --security-groups devops-sg \
+
 --user-data file://windows/windows1.ps1 \
---tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Windows-webpage}]'
+
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Windows-Webpage}]'
+
+ 
 
 echo "Launching Windows Instance 2"
+
 aws ec2 run-instances \
+
 --image-id ami-05773800d221ec923 \
+
 --count 1 \
+
 --instance-type t3.micro \
+
 --key-name devops-key \
+
 --security-groups devops-sg \
---user-data file://windows/windows2.ps1\
---tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Windows-chrome}]'
+
+--user-data file://windows/windows2.ps1 \
+
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Windows-Chrome}]'
+
+ 
+
 echo "===================================="
+
 echo "All 5 instances launched successfully"
+
 echo "===================================="
