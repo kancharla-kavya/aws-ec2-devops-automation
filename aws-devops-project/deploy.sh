dfd62a3ffafdc7c162851773849ea40c7a70d1b5
kancharla-kavya <kancharlakavya2403@gmail.com>
2026-03-30 06:17:05 +0000

    upd

--- aws-devops-project/deploy.sh
+++ aws-devops-project/deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 
- 
+
 
 echo "===================================="
 
@@ -10,7 +10,7 @@ echo "Starting AWS EC2 Automation Deploy"
 
 echo "===================================="
 
- 
+
 
 LINUX_AMI="ami-0f559c3642608c138"
 
@@ -22,49 +22,30 @@ KEY_NAME="devops-key"
 
 SECURITY_GROUP="devops-sg"
 
- 
+
 
 launch_instance() {
-
     NAME=$1
-
     AMI=$2
-
     USERDATA=$3
-
- 
 
     echo "Launching $NAME..."
 
- 
-
     INSTANCE_ID=$(aws ec2 run-instances \
-
         --image-id "$AMI" \
-
         --count 1 \
-
         --instance-type "$INSTANCE_TYPE" \
-
         --key-name "$KEY_NAME" \
-
         --security-groups "$SECURITY_GROUP" \
-
-        --user-data file://"$USERDATA" \
-
+        --user-data "file://$USERDATA" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]" \
-
         --query "Instances[0].InstanceId" \
-
-        --output text)
-
- 
+        --output text
+    )
 
     echo "$NAME launched successfully -> $INSTANCE_ID"
-
 }
 
- 
 
 launch_instance "Linux-Nginx" $LINUX_AMI linux/linux1.sh
 
@@ -76,7 +57,7 @@ launch_instance "Windows-Webpage" $WINDOWS_AMI windows/windows1.ps1
 
 launch_instance "Windows-Chrome" $WINDOWS_AMI windows/windows2.ps1
 
- 
+
 
 echo "===================================="
 
