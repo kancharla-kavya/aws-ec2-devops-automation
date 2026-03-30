68457adbf91aedf76574404457df78c491a42ea8
kancharla-kavya <kancharlakavya2403@gmail.com>
2026-03-30 06:29:17 +0000

    prev

--- aws-devops-project/deploy.sh
+++ aws-devops-project/deploy.sh
@@ -1,50 +1,71 @@
-
-cat << 'EOF' > deploy.sh
+deploy.sh
 #!/bin/bash
+
  
+
 echo "===================================="
 echo "Starting AWS EC2 Automation Deploy"
 echo "===================================="
-LINUX_AMI="ami-0f559c3642608c138"
-WINDOWS_AMI="ami-05773800d221ec923"
-INSTANCE_TYPE="t3.micro"
-KEY_NAME="devops-key"
-SECURITY_GROUP="devops-sg"
-
-launch_instance() {
-  local NAME="$1"
-  local AMI="$2"
-  local USERDATA="$3"
-
-  echo "Launching $NAME..."
-  INSTANCE_ID=$(aws ec2 run-instances \
-    --image-id "$AMI" \
-    --count 1 \
-    --instance-type "$INSTANCE_TYPE" \
-    --key-name "$KEY_NAME" \
-    --security-groups "$SECURITY_GROUP" \
-    --user-data "file://$USERDATA" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]" \
-    --query "Instances[0].InstanceId" \
-    --output text)
-  echo "$NAME launched successfully -> $INSTANCE_ID"
-
-}
-# Linux instances
-
-launch_instance "Linux-Nginx" "$LINUX_AMI" "linux/linux1.sh"
-launch_instance "Linux-Docker" "$LINUX_AMI" "linux/linux2.sh"
-launch_instance "Linux-Python" "$LINUX_AMI" "linux/linux3.sh"
 
  
 
-# Windows instances
+echo "Launching Linux Instance 1"
+aws ec2 run-instances \
+--image-id ami-0f559c3642608c138 \
+--count 1 \
+--instance-type t3.micro \
+--key-name devops-key \
+--security-groups devops-sg \
+--user-data file://linux/linux1.sh \
+--tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Linux-Nginx}]'
 
-launch_instance "Windows-Webpage" "$WINDOWS_AMI" "windows/windows1.ps1"
-launch_instance "Windows-Chrome" "$WINDOWS_AMI" "windows/windows2.ps1"
+ 
 
+echo "Launching Linux Instance 2"
+aws ec2 run-instances \
+--image-id ami-0f559c3642608c138 \
+--count 1 \
+--instance-type t3.micro \
+--key-name devops-key \
+--security-groups devops-sg \
+--user-data file://linux/linux2.sh \
+--tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Linux-docker}]'
+
+ 
+
+echo "Launching Linux Instance 3"
+aws ec2 run-instances \
+--image-id ami-0f559c3642608c138 \
+--count 1 \
+--instance-type t3.micro \
+--key-name devops-key \
+--security-groups devops-sg \
+--user-data file://linux/linux3.sh \
+--tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Linux-Python}]'
+
+ 
+
+echo "Launching Windows Instance 1"
+aws ec2 run-instances \
+--image-id ami-05773800d221ec923 \
+--count 1 \
+--instance-type t3.micro \
+--key-name devops-key \
+--security-groups devops-sg \
+--user-data file://windows/windows1.ps1 \
+--tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Windows-webpage}]'
+
+ 
+
+echo "Launching Windows Instance 2"
+aws ec2 run-instances \
+--image-id ami-05773800d221ec923 \
+--count 1 \
+--instance-type t3.micro \
+--key-name devops-key \
+--security-groups devops-sg \
+--user-data file://windows/windows2.ps1 \
+--tag-specifications 'ResourceType=instance, Tags=[{Key=Name, Value=Windows-chrome}]'
 echo "===================================="
 echo "All 5 instances launched successfully"
 echo "===================================="
-
-EOF
